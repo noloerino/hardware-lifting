2b124ba177655b7dae20801de285a7e6f46a74ca
Adwait <godbole15@gmail.com>
2021-03-29 17:54:56 -0700

    minor fix

--- verilator-examples/simple_caches_c/portconfig.h
+++ verilator-examples/simple_caches_c/portconfig.h
@@ -1,15 +1,20 @@
 #define ADD_INPUT(inputname) std::cin >> input; top->inputname = input;
 #define ADD_PRINT_PORT(portname) VL_PRINTF(" [" #portname "=%x]", top->portname); 
 
-#define SMO 0
+// Define this if the output is piped as oracle interface
+// #define SMO
+// In that case redefine this to match the required interface
+#define ADD_SMO_PRINTING_STMTS ADD_DEBUG_PRINTING_STMTS 
 
+// Configure inputs
 #define ADD_SET_INPUTS \
 	ADD_INPUT(wraddress) \
 	ADD_INPUT(wren) \
 	ADD_INPUT(data) \
 	ADD_INPUT(rdaddress)
 
-#define ADD_PRINTING_STMTS \
+// Configure DEBUGPRINT
+#define ADD_DEBUG_PRINTING_STMTS \
 	ADD_PRINT_PORT(wraddress) \
 	ADD_PRINT_PORT(wren) \
 	ADD_PRINT_PORT(data) \
